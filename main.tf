7e0cafe152925a8acbae7a1463b5ec8a6b30edcf
Vadood Bakhsh <vbakhsh@itswitch.com.au>
2019-07-01 14:42:10 +1000

    creates a vnet

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+}
+
+resource "azurerm_resource_group" "test-resource-group" {
+  name     = "temp-resource-group"
+  location = "australiasoutheast"
+
+      tags = {
+        purpose = "Demo"        
+    }
+}
+
+resource "azurerm_virtual_network" "test-network" {
+    name                = "VNet"
+    address_space       = ["192.168.58.0/24"]
+    location            = "australiasoutheast"
+    resource_group_name = "${azurerm_resource_group.test-resource-group.name}"
+
+    tags = {
+        purpose = "Demo"        
+    }
+}
+
+resource "azurerm_subnet" "test-subnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "${azurerm_resource_group.test-resource-group.name}"
+    virtual_network_name = "${azurerm_virtual_network.test-network.name}"
+    address_prefix       = "192.168.58.0/25"    
+}
+
+resource "azurerm_network_security_group" "test-nsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "australiasoutheast"
+    resource_group_name = "${azurerm_resource_group.test-resource-group.name}"
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        purpose = "Demo"        
+    }
+}
